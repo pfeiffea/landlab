Bug Fix: parcel velocity calculation

src/landlab/components/network_sediment_transporter/network_sediment_transporter.py
@@ -853,6 +853,7 @@ class NetworkSedimentTransporter(Component):
         Sarray = np.zeros(self._num_parcels)
         Harray = np.zeros(self._num_parcels)
         Larray = np.zeros(self._num_parcels)
+        Warray = np.zeros(self._num_parcels)
 
         D50_activearray = np.full(self._num_parcels, np.nan)
         active_layer_thickness_array = np.full(self._num_parcels, np.nan)
@@ -906,6 +907,7 @@ class NetworkSedimentTransporter(Component):
             Sarray[Linkarray == i] = self._grid.at_link["channel_slope"][i]
             Harray[Linkarray == i] = self._grid.at_link["flow_depth"][i]
             Larray[Linkarray == i] = self._grid.at_link["reach_length"][i]
+            Warray[Linkarray == i] = self._grid.at_link["channel_width"][i]
             active_layer_thickness_array[Linkarray == i] = self._active_layer_thickness[
                 i
             ]
@@ -938,18 +940,39 @@ class NetworkSedimentTransporter(Component):
             (1 - (0.894 / np.sqrt(tautaur_cplx.real[tautaur >= 1.35]))), 4.5
         )
 
+        self._Wstari = W # To allow check against WC fig
+
         active_parcel_idx = Activearray == _ACTIVE
 
-        # compute parcel virtual velocity, m/s
-        self._pvelocity[active_parcel_idx] = (
-            W.real[active_parcel_idx]
-            * (tau[active_parcel_idx] ** (3.0 / 2.0))
-            * frac_parcel[active_parcel_idx]
-            / (self._fluid_density ** (3.0 / 2.0))
-            / self._g
-            / R[active_parcel_idx]
-            / active_layer_thickness_array[active_parcel_idx]
-        )
+        # Calculate sediment fluxes
+        qbi = np.zeros(self._num_parcels)
+
+        qbi[active_parcel_idx] = (W.real[active_parcel_idx]
+               * frac_parcel[active_parcel_idx]
+               * (tau[active_parcel_idx]/self._fluid_density)**(3/2)
+               / (Rhoarray[active_parcel_idx]/self._fluid_density -1)
+               / self._g
+        ) # (m2/s) WC eqn 2
+
+        self._qbi = qbi 
+
+        self._frac_parcel = frac_parcel
+
+        qb = aggregate_items_as_sum(
+            self._parcels.dataset["element_id"].values[:, -1].astype(int),
+            qbi,
+            size=self._grid.number_of_links,
+        ) # total sediment flux per unit width, m2/s
+
+        self._qb = qb
+
+        self._pvelocity[active_parcel_idx]=(
+            Larray[active_parcel_idx]
+            *qbi[active_parcel_idx]
+            *Warray[active_parcel_idx]
+            / Volarray[active_parcel_idx] 
+
+        )# (m/s)# Bug fix 4/2026
 
         self._pvelocity[np.isnan(self._pvelocity)] = 0.0
 